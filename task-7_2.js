4c3744666c1d103829c5dcf0bc44d17c3e5d88ee
loGOshy <65897894+loGOshy@users.noreply.github.com>
2021-02-25 23:33:12 +0200

    Update task-7_2.js

--- task-7_2.js
+++ task-7_2.js
@@ -6,3 +6,21 @@ const ingredients = [
     'Зелень',
     'Приправы',
   ];
+
+
+  const ingredientsListRef = document.querySelector('#ingredients');
+  // console.log(ingredientsListRef);
+
+  const ingredientItem = (name) => {
+  const ingredientElement = document.createElement('li');
+  ingredientElement.textContent = name;
+
+  return ingredientElement;
+  };
+
+  const renderIngredienstList = (ingredientsList) => {
+  const ingredientsElements = ingredientsList.map((ingredient) => ingredientItem(ingredient));
+  ingredientsListRef.append(...ingredientsElements);
+  } ;
+
+renderIngredienstList(ingredients);
